60726c019a8b88dc3ac31c42c7a53c5cfd11424c
LopezIgnacio2201 <lopezignacio2201@gmail.com>
2022-05-17 21:02:37 -0300

    clase 2

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+    return <header>
+        <h1>Proyecto Coder</h1>
+        
+        <nav>
+        <p>Enlace 1</p>
+        <p>Enlace 2</p>
+        <p>Enlace 3</p>
+        </nav>
+    </header>
+}
+
+export default Navbar
